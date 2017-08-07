751cbeec829940ba63e52f41b27bbc9dc9a4bf80
JongKwang <kim@jongkwang.com>
2017-08-08 02:27:46 +0900

    Create Calculator_Sample.java
    It's a sample for training member.
    It build with very simple code.

---
+++ Calculator_Sample.java
@@ -0,0 +1,30 @@
+package com.mycompany.test;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		int a = 4;
+		int b = 2;
+		System.out.println(sum(a,b));
+		System.out.println(minus(a,b));
+		System.out.println(multi(a,b));
+		System.out.println(division(a,b));
+	}
+	
+	private static int sum(int a, int b) {
+		return a + b;
+	}
+	
+	private static int minus(int a, int b) {
+		return a - b;
+	}
+	
+	private static int multi(int a, int b) {
+		return a * b;
+	}
+	
+	private static float division(int a, int b) {
+		return a / b;
+	}
+
+}
